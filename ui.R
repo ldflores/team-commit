cc39c04f40b2f5269fe78a112365924ccb1c3f18
Stefani Sanchez <sanch397@uw.edu>
2017-12-02 15:04:46 -0800

    Add ui

---
+++ ui.R
@@ -0,0 +1,36 @@
+library(shiny)
+library(rsconnect)
+
+
+# Show a tabset that includes a plot, summary, and
+# table view of the generated distribution
+my.ui <- fluidPage(
+  navbarPage("stats!",
+             tabPanel("Summary", 
+                      mainPanel(
+                        includemd("README.md")
+                      ),
+                      tabPanel("Salary by Major",
+                               sidebarLayout(
+                                 sidebarPanel(
+                                   radioButtons("dist", "Select major category:",
+                                                c(majors$Major_category))
+                                 )
+                               ),
+                               mainPanel(
+                                 plotOutput("plot")
+                               ),
+                               tabPanel("Average Salary", 
+                                        sidebarLayout(
+                                          sidebarPanel(
+                                            
+                                          )
+                                        ),
+                                        mainPanel(
+                                          plotOutput("plot")
+                                        )
+                               )
+                      )
+             )
+  )
+)
