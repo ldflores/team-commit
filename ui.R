e396230238912a5c33bb66af22cc0c2b0d72e9c2
Stefani Sanchez <sanch397@uw.edu>
2017-12-02 15:16:15 -0800

    Fixes #7, format

--- ui.R
+++ ui.R
@@ -6,31 +6,31 @@ library(rsconnect)
 # table view of the generated distribution
 my.ui <- fluidPage(
   navbarPage("stats!",
-             tabPanel("Summary", 
-                      mainPanel(
-                        includemd("README.md")
-                      ),
-                      tabPanel("Salary by Major",
-                               sidebarLayout(
-                                 sidebarPanel(
-                                   radioButtons("dist", "Select major category:",
-                                                c(majors$Major_category))
-                                 )
-                               ),
-                               mainPanel(
-                                 plotOutput("plot")
-                               ),
-                               tabPanel("Average Salary", 
-                                        sidebarLayout(
-                                          sidebarPanel(
+    tabPanel("Summary", 
+        mainPanel(
+          includemd("README.md")
+        ),
+        tabPanel("Salary by Major",
+          sidebarLayout(
+            sidebarPanel(
+              radioButtons("dist", "Select major category:",
+                c(majors$Major_category))
+            )
+          ),
+          mainPanel(
+            plotOutput("plot")
+          ),
+        tabPanel("Average Salary", 
+          sidebarLayout(
+            sidebarPanel(
                                             
-                                          )
-                                        ),
-                                        mainPanel(
-                                          plotOutput("plot")
-                                        )
-                               )
-                      )
-             )
+            )
+          ),
+          mainPanel(
+            plotOutput("plot")
+          )
+        )
+      )
+    )
   )
 )
