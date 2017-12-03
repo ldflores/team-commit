6e8ddef22412784234fdcdd0dc5c8d20f712b430
ldflores <008luisf@gmail.com>
2017-12-02 16:05:50 -0800

    minor fix

--- scripts/server.R
+++ scripts/server.R
@@ -2,6 +2,7 @@ library(dplyr)
 library(ggplot2)
 
 # get data into dataframe
+
 data <- read.csv(file = 'all-ages.csv', stringsAsFactors = FALSE, header = TRUE)
 
 # compute salary variability column to be used in graph
