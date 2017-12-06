475fec2f1eed8d25922b1a2d9be17b5598be5897
mauracassidy <mcass@uw.edu>
2017-12-05 23:47:46 -0800

    added analysis to boxplot graph

--- scripts/ui.R
+++ scripts/ui.R
@@ -39,7 +39,17 @@ shinyUI(fluidPage(
                                   selected = "Agriculture & Natural Resources")
             ),
             mainPanel(
-              plotOutput("box")
+              plotOutput("box"),
+              p("This graph shows the range of salaries for different major categories after
+                graduation. It is beneficial to compare the median salaries of each major
+                category, it gives a better idea of future career salaries. When comparing all
+                of the major categories against eachother, the health category has the highest
+                median out of all the major categories. A student looking for a high paying salary 
+                should look into the health majors. The engineering major category has the
+                greatest difference between the highest and the lowest salaries. A student looking 
+                to major in a field of engineering should know that it has a high variance in salary.
+                The major categories with the lowest median salaries are education majors. A student 
+                looking tp pursue education will find this information valuable. ")
             )
          )
        )
